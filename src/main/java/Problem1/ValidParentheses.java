ce1728ea40ed42fe9f09685b0de242d1ce582719
cici1 <1249819124@qq.com>
2020-10-25 23:31:01 -0700

    hw3

--- src/main/java/Problem1/ValidParentheses.java
+++ src/main/java/Problem1/ValidParentheses.java
@@ -1,11 +1,53 @@
 package Problem1;
+import java.util.*;
 
-public class ValidParentheses {
+public class ValidParentheses
+{
+    public static boolean isValid(String str)
+    {
+        Deque<Character> stack
+                = new ArrayDeque<Character>();
 
-    // Do not change signature (function name, parameters, return type)
-    public static boolean isValid(String str) {
-        // homework
-        // !!! must use ArrayStack or LinkedListStack from problem 1
-        return false; // place holder
+        for (int i = 0; i < str.length(); i++)
+        {
+            char x = str.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{')
+            {
+
+                stack.push(x);
+                continue;
+            }
+
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x)
+            {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+
+        return (stack.isEmpty());
     }
+
+
+
 }
